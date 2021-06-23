8320d2da3ffb04e0790eabedf16400da2748059d
skipstorm <gcossu@gmail.com>
2021-06-23 18:00:37 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 (function($){
   $(document).ready(function(){
+    
+    var datiUtente;
+    
     $('#datiUtente').on('change', function(){
-      console.log($(this).val().split("\n"));
+      
     });
+    
+    function makeUserPreview() {
+      var size = parseInt($('#datiUtenteN').val());
+      datiUtente = $('#datiUtente').val().split("\n").map(function(line){ return line.split("\t").slice(0, size)});
+      $('.datiUtentePreview').empty();
+      var table = '<table class="table table-striped">';
+      for(let d of datiUtente) {
+        table += '<tr><td>'+(d.join('</td><td>')+'</td></tr>');
+      }
+      table += '</table>';
+      $('.datiUtentePreview').append(table);
+    }
   });
 })(jQuery);
